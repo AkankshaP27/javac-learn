bd15f1a375cbaf6a027e4f7aa7801e3896b4278a
AkankshaP27 <akankshapawar122@gmail.com>
2023-01-23 21:24:00 +0530

    First Commit

---
+++ DoWhileTask.java
@@ -0,0 +1,12 @@
+public class DoWhileTask
+{
+    public static void main(String args[])
+    {
+        int i=1,num=4,mul;
+        do{
+            System.out.println("mul:" +num*i);
+            i++;
+        }
+        while(i<=10);
+    }
+}
